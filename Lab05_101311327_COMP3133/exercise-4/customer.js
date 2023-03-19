3f861b5994db8ed08b1994844536078a7dd5a4a6
angelaefremova <35670694+angelaefremova@users.noreply.github.com>
2023-03-19 12:27:25 -0400

    Lab 5 added

---
+++ Lab05_101311327_COMP3133/exercise-4/customer.js
@@ -0,0 +1,20 @@
+"use strict";
+exports.__esModule = true;
+exports.Customer = void 0;
+var Customer = /** @class */ (function () {
+    function Customer(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    Customer.prototype.greeter = function () {
+        console.log("Hello ".concat(this.firstName, " ").concat(this.lastName));
+    };
+    Customer.prototype.GetAge = function () {
+        console.log("Customer age is ".concat(this.age));
+    };
+    return Customer;
+}());
+exports.Customer = Customer;
+var customer = new Customer("John", "Smith", 28);
+customer.greeter();
